59cc9ee4b85a1fc44e653d371026f9d4d3cad83a
vishwabalamurugan2013 <vishwabalamurugan2013@gmail.com>
2026-05-17 13:09:46 +0000

    fix: Correctly call vite-plugin-monaco-editor

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({mode}) => {
     plugins: [
       react(),
       tailwindcss(),
-      monacoEditorPlugin({
+      (monacoEditorPlugin as any).default({
         languageWorkers: ['editorWorkerService', 'css', 'html', 'json', 'typescript'],
       }),
     ],
