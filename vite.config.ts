436a6e544ebe0752458438cfcf94bb3d71c6a90a
vishwabalamurugan2013 <vishwabalamurugan2013@gmail.com>
2026-05-17 12:56:34 +0000

    Fix: Correctly configure vite-plugin-monaco-editor

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
-
-const monacoEditorPlugin = ((await import('vite-plugin-monaco-editor')) as any).default;
+import { defineConfig, loadEnv } from 'vite';
+import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
