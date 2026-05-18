3bc0d041cd5064baa4ad9dfe59e6579638ce3e26
vishwabalamurugan2013 <vishwabalamurugan2013@gmail.com>
2026-05-18 04:56:30 +0000

    Fix: Correct build error and update vulnerable dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
-import { pyodide } from 'vite-plugin-pyodide';
+import pyodide from 'vite-plugin-pyodide';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
